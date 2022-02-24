1a2cdb034e64d0c3386e9d93671f8a8ce543df54
morsatha <morsatha123@gmail.com>
2022-02-24 23:51:01 +0600

    lastpar

--- js/app.js
+++ js/app.js
@@ -13,6 +13,25 @@ function updateProductNumber(product, price, isIncreasing) {
     productInput.value = productNumber;
     const productTotal = document.getElementById(product + '-total');
     productTotal.innerText = productNumber * price;
+    //calculatorTotal function Call
+    calculatorTotal();
+}
+function getInputValue(product) {
+    const phoneInput = document.getElementById(product + '-number');
+    let phoneNumber = parseInt(phoneInput.value);
+    return phoneNumber;
+}
+function calculatorTotal() {
+    const phoneTotal = getInputValue('phone') * 1219;
+    const caseTotal = getInputValue('case') * 59;
+    const subTotal = phoneTotal + caseTotal;
+    const tax = subTotal / 10;
+    const total = subTotal + tax;
+    document.getElementById('subtotal-taka').innerText = subTotal;
+    document.getElementById('tax-amount').innerText = tax;
+    document.getElementById('total').innerText = total;
+
+    // console.log(subTotal)
 }
 
 document.getElementById('phone-plase').addEventListener('click', function () {
@@ -32,3 +51,4 @@ document.getElementById('case-mines').addEventListener('click', function () {
 
     updateProductNumber('case', 59, false);
 });
+
